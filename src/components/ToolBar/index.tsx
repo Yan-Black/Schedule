5e6cd061af8f7f947eca2e08b510ada9471c70bb
Liudmila_Arzamasova <liudmila.arzamasova@gmail.com>
2020-09-09 01:58:58 +0300

    styles: footer header tabs

--- src/components/ToolBar/index.tsx
+++ src/components/ToolBar/index.tsx
@@ -1,8 +1,22 @@
 import * as React from 'react';
 
+import { HeartTwoTone, DownloadOutlined, SettingOutlined } from '@ant-design/icons';
+
 import './index.scss';
 
 const ToolBar: React.FC = () => {
-  return <div className="toolbar  container">icons (heart, download, settings)</div>;
+  return (
+    <div className="toolbar">
+      <a href="/">
+        <HeartTwoTone twoToneColor="#eb2f96" />
+      </a>
+      <a href="/">
+        <DownloadOutlined />
+      </a>
+      <a href="/">
+        <SettingOutlined style={{ color: 'black' }} />
+      </a>
+    </div>
+  );
 };
 export default ToolBar;
