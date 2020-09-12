371968924b63343734b23ea23b5bbda7d26c902a
Kostya Gorodnichuk <coolkostya@gmail.com>
2020-09-12 21:25:59 +0300

    fix: code refactor role component

--- src/components/Header/RoleSwitcher/index.tsx
+++ src/components/Header/RoleSwitcher/index.tsx
@@ -1,24 +1,20 @@
 import * as React from 'react';
-import { RootState } from 'store';
 import './index.scss';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { switchRole } from 'reducers/role';
 import { userRoles } from '../../../constants';
 
-
 const RoleSwitcher: React.FC = () => {
   const dispatch = useDispatch();
-  const currentRole = useSelector((state: RootState) => state.role.currentRole)
-
   const changeHandler = (e: any) => {
     dispatch(switchRole(e.target.value));
   };
-  const arrOfRoleOptionNode = userRoles.map((role: string) => {
+  const arrOfRoleNodes = userRoles.map((role: string) => {
     return <option className="roleSwitcher__point" value={role} key={role}>{role[0].toUpperCase() + role.slice(1)}</option>
   })
   return (
-    <select className='header__roleSwitcher roleSwitcher' onChange={(e) => changeHandler(e)}>
-      {arrOfRoleOptionNode}
+    <select className='header__roleSwitcher roleSwitcher' onChange={changeHandler}>
+      {arrOfRoleNodes}
     </select>
   )
 };
