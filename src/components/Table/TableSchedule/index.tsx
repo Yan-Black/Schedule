536a15cbcc00b49e750841005ea484a9a52bd494
Stepanenko Oksana <lactivo4ka@gmail.com>
2020-09-17 16:27:57 +0300

    feat: add table placeholder while data is loading

--- src/components/Table/TableSchedule/index.tsx
+++ src/components/Table/TableSchedule/index.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
-import { Table } from 'antd';
+import { Table, Skeleton } from 'antd';
 import './index.scss';
+import { useSelector } from 'react-redux';
+import { RootState } from 'store';
 import { WeekData } from './models';
 import expandedRow from './ExpandedRow';
 
 const TableSchedule: React.FC = () => {
+  const loading = useSelector((state: RootState) => state.events.loading);
+
   const columns = [
     {
       title: 'RS School Schedule',
@@ -23,15 +27,17 @@ const TableSchedule: React.FC = () => {
   }
 
   return (
-    <Table<WeekData>
-      columns={columns}
-      expandable={{ expandedRowRender: (record) => record.weekData }}
-      defaultExpandedRowKeys={[1]}
-      dataSource={data}
-      pagination={false}
-      rowClassName={(record, index) => `currentWeek${index}`}
-      scroll={{ y: 500 }}
-    />
+    <Skeleton loading={loading}>
+      <Table<WeekData>
+        columns={columns}
+        expandable={{ expandedRowRender: (record) => record.weekData }}
+        defaultExpandedRowKeys={[1]}
+        dataSource={data}
+        pagination={false}
+        rowClassName={(record, index) => `currentWeek${index}`}
+        scroll={{ y: 500 }}
+      />
+    </Skeleton>
   );
 };
 
