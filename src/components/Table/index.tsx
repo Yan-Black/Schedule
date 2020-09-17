7de20455ee672310cf9af6459e8f53b74703f611
Yan Andryshkin <yan.7feb@gmail.com>
2020-09-17 20:21:23 +0300

    feat: add skeleton preloader into tabe component

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from 'store';
+import { Skeleton } from 'antd';
 import TableSchedule from './TableSchedule';
 import './index.scss';
 
 const Table: React.FC = () => {
   const isLoading = useSelector((state: RootState) => state.events.loading);
-  return isLoading ? <p>loading...</p> : <TableSchedule />;
+  return isLoading ? <Skeleton active /> : <TableSchedule />;
 };
 
 export default Table;
