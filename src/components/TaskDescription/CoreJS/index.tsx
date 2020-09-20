0cdd9a0c50c0d8cf3547f1e41739f0b780a50d9d
Nadya Marchanka <nadia.besarab@gmail.com>
2020-09-20 21:50:53 +0300

    feat: add coreJS typetask template

--- src/components/TaskDescription/CoreJS/index.tsx
+++ src/components/TaskDescription/CoreJS/index.tsx
@@ -1,6 +1,7 @@
 import './index.scss';
 import * as React from 'react';
 import { useSelector } from 'react-redux';
+import { Menu } from 'antd';
 
 import { RootState } from 'store';
 
@@ -12,8 +13,56 @@ const CoreJS: React.FC = () => {
   }
 
   return (
-    <>
-      <div>CoreJS Task</div>
+      <>
+      <div className="task-description-standart">
+        <div className="task-description-nav">
+          <Menu
+            style={{ width: 256 }}
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            mode="inline"
+          >
+            <Menu.Item key="1">Описание заданий</Menu.Item>
+            <Menu.Item key="2">Требования для утановки</Menu.Item>
+            <Menu.Item key="3">Как сабмитнуть</Menu.Item>
+            <Menu.Item key="3">Материалы</Menu.Item>
+          </Menu>
+        </div>
+        <div className="task-description-area">
+          <h2 className="task-main-headline">Описание заданий</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim.
+          </p>
+          <h2 className="task-main-headline">Требования для установки</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim.
+          </p>
+          <h2 className="task-main-headline">Как сабмитнуть</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim.
+          </p>
+          <h2 className="task-main-headline">Материалы</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
