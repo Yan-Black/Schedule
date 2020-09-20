6ea948be509a0ca5d2274920d15059c92739f1bb
Nadya Marchanka <nadia.besarab@gmail.com>
2020-09-20 18:08:40 +0300

    feat: add meetup and codewars typetask templates

--- src/components/TaskDescription/Codewars/index.tsx
+++ src/components/TaskDescription/Codewars/index.tsx
@@ -1,6 +1,7 @@
 import './index.scss';
 import * as React from 'react';
 import { useSelector } from 'react-redux';
+import { Menu } from 'antd';
 
 import { RootState } from 'store';
 
@@ -13,7 +14,64 @@ const Codewars: React.FC = () => {
 
   return (
     <>
-      <div>Codewars Task</div>
+      <div className="task-description-standart">
+        <div className="task-description-nav">
+          <Menu
+            style={{ width: 256 }}
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            mode="inline"
+          >
+            <Menu.Item key="1">Список задач</Menu.Item>
+            <Menu.Item key="2">Критерии оценки</Menu.Item>
+            <Menu.Item key="3">Как сабмитнуть</Menu.Item>
+            <Menu.Item key="4">Auto-check</Menu.Item>
+            <Menu.Item key="5">Материалы</Menu.Item>
+          </Menu>
+        </div>
+        <div className="task-description-area">
+          <h2 className="task-main-headline">Список задач</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim.
+          </p>
+          <h2 className="task-main-headline">Критерии оценки</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim.
+          </p>
+          <h2 className="task-main-headline">Как сабмитнуть</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim.
+          </p>
+          <h2 className="task-main-headline">Auto-check</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim.
+          </p>
+          <h2 className="task-main-headline">Материалы</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/TaskDescription/Meetup/index.tsx
+++ src/components/TaskDescription/Meetup/index.tsx
@@ -1,6 +1,7 @@
 import './index.scss';
 import * as React from 'react';
 import { useSelector } from 'react-redux';
+import { Menu } from 'antd';
 
 import { RootState } from 'store';
 
@@ -13,7 +14,46 @@ const Meetup: React.FC = () => {
 
   return (
     <>
-      <div>Meetup Task</div>
+      <div className="task-description-standart">
+        <div className="task-description-nav">
+          <Menu
+            style={{ width: 256 }}
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            mode="inline"
+          >
+            <Menu.Item key="1">Программа</Menu.Item>
+            <Menu.Item key="2">Язык</Menu.Item>
+            <Menu.Item key="3">Место встречи</Menu.Item>
+          </Menu>
+        </div>
+        <div className="task-description-area">
+          <h2 className="task-main-headline">Программа</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim.
+          </p>
+          <h2 className="task-main-headline">Язык выступления</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim.
+          </p>
+          <h2 className="task-main-headline">Место встречи</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
